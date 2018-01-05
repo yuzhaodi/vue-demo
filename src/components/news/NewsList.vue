859457883dfbffb2a34ea7def3d07a7c3cf2ffbc
yuzhaodi <3022912976@qq.com>
2018-01-05 15:05:12 +0800

    11

---
+++ src/components/news/NewsList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div><h3>新闻列表</h3></div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
+
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -14,7 +16,8 @@ var router = new VueRouter({
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/shopcar', component: ShopcarContainer },
-    { path: '/search', component: SearchContainer }
+    { path: '/search', component: SearchContainer },
+    {path:'/home/newslist',component:NewsList}
   ],
   linkActiveClass:' mui-active'
 })
