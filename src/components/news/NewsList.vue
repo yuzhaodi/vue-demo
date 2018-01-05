59450ff041c9288cccfc4be91c3a894214d1b365
yuzhaodi <3022912976@qq.com>
2018-01-05 15:22:57 +0800

    aa

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,12 +1,63 @@
 <template>
-  <div><h3>新闻列表</h3></div>
+  <div>
+    <ul class="mui-table-view">
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars0.githubusercontent.com/u/34207006?s=40&v=4">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                <span>2012-12-12 12:12:12</span>
+                <span>点击量:0次</span>
+              </p>
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars0.githubusercontent.com/u/34207006?s=40&v=4">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                <span>2012-12-12 12:12:12</span>
+                <span>点击量:0次</span>
+              </p>
+						</div>
+					</a>
+				</li>
+        <li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars0.githubusercontent.com/u/34207006?s=40&v=4">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                <span>2012-12-12 12:12:12</span>
+                <span>点击量:0次</span>
+              </p>
+						</div>
+					</a>
+				</li>
+			</ul>
+  </div>
 </template>
 
 <script>
   
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+  .mui-table-view{
+    li{
+      h1{
+        font-size: 14px
+      }
+    }
+    .mui-ellipsis{
+      font-size: 12px;
+      color: #226aff;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 </style>
 
