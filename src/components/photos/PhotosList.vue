7ca0997fb8ccc962acd3c1e416650a6a48c24eb1
yuzhaodi <3022912976@qq.com>
2018-01-06 12:41:06 +0800

    vv

--- src/components/photos/PhotosList.vue
+++ src/components/photos/PhotosList.vue
@@ -30,10 +30,25 @@
 
 
 <script>
-  
+  import mui from '../../lib/mui/js/mui.min.js'
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    mounted() {
+   // 需要在组件的 mounted 事件钩子中，注册 mui 的 scroll 滚动事件
+     mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+   });
+   }
+  }
 </script>
 
 
 <style lang="scss" scoped>
-
+*{
+touch-action:pan-y
+}
 </style>
