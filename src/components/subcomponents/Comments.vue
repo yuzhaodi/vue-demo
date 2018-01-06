d0a9d80fccd1cbb941d548f5e707d4ae7d2133a7
yuzhaodi <3022912976@qq.com>
2018-01-06 09:29:43 +0800

    ff

--- src/components/subcomponents/Comments.vue
+++ src/components/subcomponents/Comments.vue
@@ -5,12 +5,12 @@
     <textarea placeholder="快点输入,别墨迹" maxlength="120"></textarea>
     <mt-button type="primary" size='large'>发表评论</mt-button>
     <div class="cmt-list">
-      <div class="cmt-item">
+      <div class="cmt-item" v-for="(item,i) in comments" :key="item.add_time">
         <div class="cmt-title">
-          这个写用户信息的
+          第{{ i+1 }}楼&nbsp;&nbsp;用户：{{item.user_name}}&nbsp;&nbsp;发表时间:{{item.add_time | dateformat}}
         </div>
         <div class="cmt-body">
-          这里写用户留言的
+          {{item.content==='undefined'?'此用户懒得很,没写啥':item.content}}
         </div>
       </div>
     </div>
@@ -19,7 +19,30 @@
 </template>
 
 <script>
-  
+import {Toast} from 'mint-ui'
+  export default{
+    data(){
+      return {
+        pageIndex:1,
+        comments:[]
+      }
+    },
+    created(){
+      this.getComments()
+    },
+    methods:{
+      getComments(){
+        this.$http.get('api/getcomments/'+this.id+'?pageindex='+this.pageIndex).then(result=>{
+          if(result.body.status===0){
+            this.comments=result.body.message
+          }else{
+            Toast('评论加载失败')
+          }
+        })
+      }
+    },
+    props:['id']
+  }
 </script>
 
 
