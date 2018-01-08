6c473734a3a150bfdeda44b426ecec159ed4727a
yuzhaodi <3022912976@qq.com>
2018-01-08 15:43:17 +0800

    i

--- src/components/goods/Goodsinfo.vue
+++ src/components/goods/Goodsinfo.vue
@@ -10,11 +10,16 @@
     </div>
 
     <!-- 商品购买 -->
-    <div class="mui-card">``
-      <div class="mui-card-header">页眉</div>
+    <div class="mui-card">
+      <div class="mui-card-header">{{goodinfo.title}}</div>
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
-          包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）
+          <p class="price">
+            市场价:<del>￥{{goodinfo.market_price}}</del>&nbsp;&nbsp;销售价：<span class="now_price">￥{{goodinfo.sell_price}}</span>
+          </p>
+          <p>购买数量：<numbox></numbox></p>
+          <mt-button type="primary" size='small'>立即购买</mt-button>
+          <mt-button type="danger" size='small'>加入购物车</mt-button>
         </div>
       </div>
     </div>
@@ -23,25 +28,33 @@
       <div class="mui-card-header">页眉</div>
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
-          包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）
+          <p>商品货号：{{goodinfo.goods_no}}</p>
+          <p>库存情况：{{goodinfo.stock_quantity}}</p>
+          <p>上架时间：{{goodinfo.add_time | dateformat}}</p>
         </div>
       </div>
-      <div class="mui-card-footer">页脚</div>
+      <div class="mui-card-footer">
+        <mt-button type="primary" size="large" plain>图文介绍</mt-button>
+        <mt-button type="danger" size="large" plain>商品评论</mt-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import swiper from '../subcomponents/Lunbo.vue'
+import numbox from '../subcomponents/NumberBox.vue'
   export default {
     data(){
       return{
         id:this.$route.params.id,
-        lunbotu:[]
+        lunbotu:[],
+        goodinfo:{}
       }
     },
     created(){
-      this.getLunbo()
+      this.getLunbo(),
+      this.getGoodsinfo()
     },
     methods:{
       getLunbo(){
@@ -53,10 +66,18 @@ import swiper from '../subcomponents/Lunbo.vue'
             this.lunbotu=result.body.message
           }
         })
+      },
+      getGoodsinfo(){
+        this.$http.get('api/goods/getinfo/'+this.id).then(result=>{
+          if(result.body.status===0){
+            this.goodinfo=result.body.message[0]
+          }
+        })
       }
     },
     components:{
-      swiper
+      swiper,
+      numbox
     }
   }
 </script>
@@ -67,5 +88,16 @@ import swiper from '../subcomponents/Lunbo.vue'
   .goodsinfo-container {
   background-color: #eee;
   overflow: hidden;
+  }
+  .now_price {
+    color: red;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .mui-card-footer {
+    display: block;
+    button {
+      margin: 15px 0;
+    }
   }
 </style>
---
+++ src/components/subcomponents/NumberBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mui-numbox" data-numbox-step='1' data-numbox-min='0' data-numbox-max='9'>
+      <button class="mui-btn mui-numbox-btn-minus" type="button">-</button>
+      <input class="mui-numbox-input" type="number" />
+      <button class="mui-btn mui-numbox-btn-plus" type="button">+</button>
+    </div>
+</template>
+
+
+
+<script>
+import mui from '../../lib/mui/js/mui.min.js'
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    mounted(){
+      mui('.mui-numbox').numbox()
+    }
+  }
+
+</script>
+
+
+
+<style lang="scss" scoped>
+</style>
